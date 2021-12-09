a3dcf0c02906f0166ce79c587b5c8a8a7e9dba34
RaiNote <Timothy.Lutz@gmx.de>
2021-12-09 10:35:43 +0100

    Update Function definitions

--- src/caesar/caesar.h
+++ src/caesar/caesar.h
@@ -7,11 +7,13 @@
 #include <iostream>
 namespace caesar {
 
-    std::shared_ptr<std::string> decode(const std::string &string, int shift);
-    std::shared_ptr<std::string> encode(const std::string &string, int shift);/*
-    int rateSolution(const std::string &string, dictionary &dictionary);
-    int rateSolutionAccordingToWordList(const std::string &string, dictionary &dictionary);*/
-    int rateSolutionAccordingToVocals(const std::string &string);
-    int rateSolutionAccordingToTypicalCharacterSequence(const std::string &string);
+	std::shared_ptr<std::string> decode(const std::string& string, int shift);
+	std::shared_ptr<std::string> encode(const std::string& string, int shift);
+	int rateSolution(const std::string& string);
+	int rateSolutionAccordingToWordList(const std::string& string);
+	int rateSolutionAccordingToVocals(const std::string& string);
+	int rateSolutionAccordingToTypicalCharacterSequence(const std::string& string);
 
+	void loadDictionary();
+	int getValueOfWord(const std::string& string, int start, int end);
 }// namespace caesar
